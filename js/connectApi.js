9c97b0c1d087c47bb3cdc75bd3f8a666468ccaf8
Rodrigo Borgognoni <rodrigo.borgognoni@jauserve.com.br>
2024-10-09 15:41:34 -0300

    create function videoCard

---
+++ js/connectApi.js
@@ -0,0 +1,11 @@
+async function listaVideos() {
+    const conexaoApi = await fetch('http://localhost:3000/videos');
+    const apiResponse = await conexaoApi.json();
+
+    return apiResponse;
+
+}
+
+export const apiResponse = {
+    listaVideos
+}
